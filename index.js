f7d1d01b11ed99b216a00594dc631573fbb584d8
jnrdhn <nick149e@gmail.com>
2021-12-07 14:03:47 +0530

    done

--- index.js
+++ index.js
@@ -49,7 +49,11 @@ scheme = {
 };
 
 const main = async () => {
+	const chess = new Chess();
 	const pgn = `1.e4 Nf6 2.e5 Nd5 3.d4 d6 4.Nf3 g6 5.Bc4 Nb6 6.Bb3 Bg7 7.Qe2 Nc6 8.O-O O-O 9.h3 a5 10.a4 dxe5 11.dxe5 Nd4 12.Nxd4 Qxd4 13.Re1 e6 14.Nd2 Nd5 15.Nf3 Qc5 16.Qe4 Qb4 17.Bc4 Nb6 18.b3 Nxc4 19.bxc4 Re8 20.Rd1 Qc5 21.Qh4 b6 22.Be3 Qc6 23.Bh6 Bh8 24.Rd8 Bb7 25.Rad1 Bg7 26.R8d7 Rf8 27.Bxg7 Kxg7 28.R1d4 Rae8 29.Qf6+ Kg8 30.h4 h5 31.Kh2 Rc8 32.Kg3 Rce8 33.Kf4 Bc8 34.Kg5 1-0`;
+	chess.load_pgn(pgn);
+	let ph = chess.history({ verbose: true });
+	const lmPiece = ph[ph.length - 1].piece;
 
 	const progressionLayerBuffer = await getBlurredProgression(pgn);
 	saveImageFromBuffer(progressionLayerBuffer, "progressionLayer");
@@ -76,7 +80,7 @@ const main = async () => {
 			console.log(err);
 		});
 
-	loadImage("./inputs/knight-bg.png")
+	loadImage(`./inputs/${lmPiece}_bg.png`)
 		.then(async (mask) => {
 			await frame2Ctx.drawImage(mask, 0, 0);
 			// saveImageFromCanvas(frame2, "mask");
@@ -119,7 +123,7 @@ const main = async () => {
 		// saveImageFromCanvas(frame, "textAdded");
 	});
 
-	loadImage("./inputs/knight-bg.png")
+	loadImage(`./inputs/${lmPiece}_bg.png`)
 		.then(async (knightbg) => {
 			await frameCtx.drawImage(knightbg, 263, 263, 780, 780);
 			// saveImageFromCanvas(frame, "withk");
@@ -128,7 +132,7 @@ const main = async () => {
 			console.log(err);
 		});
 
-	loadImage("./output/pieceMask.png")
+	loadImage(`./output/pieceMask.png`)
 		.then(async (knight) => {
 			await frameCtx.drawImage(knight, 263, 263, 780, 780);
 			// saveImageFromCanvas(frame, "withk");
@@ -137,10 +141,10 @@ const main = async () => {
 			console.log(err);
 		});
 
-	loadImage("./inputs/knight-fg.png")
+	loadImage(`./inputs/${lmPiece}_fg.png`)
 		.then(async (knightfg) => {
 			await frameCtx.drawImage(knightfg, 263, 263, 780, 780);
-			saveImageFromCanvas(frame, "finalNFT");
+			saveImageFromCanvas(frame, "NFT");
 		})
 		.catch((err) => {
 			console.log(err);
